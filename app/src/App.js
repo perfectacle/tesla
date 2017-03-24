2151f074b9f8bf176903d3cd123cd4b7162a7b2b
perfectacle <perfectacle@naver.com>
2017-03-24 16:06:51 +0900

    init

--- app/src/App.js
+++ app/src/App.js
@@ -2,33 +2,10 @@ import React from 'react/lib/React';
 import Component from 'react/lib/ReactComponent';
 import './App.css';
 
-import Header from './components/Header/Header';
-import TeslaBattery from './containers/TeslaBattery/TeslaBattery';
-
-const counterDefaultVal = {
-  speed: {
-    title: 'Speed',
-    unit: 'mph',
-    step: 5,
-    min: 45,
-    max: 70
-  },
-  temperature: {
-    title: 'Outside Temperature',
-    unit: '°',
-    step: 10,
-    min: -10,
-    max: 40
-  }
-};
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header/>
-        <TeslaBattery counterDefaultVal={counterDefaultVal}/>
-      </div>
+      <h1>시작!</h1>
     );
   }
 }
--- app/src/index.js
+++ app/src/index.js
@@ -1,4 +1,4 @@
-if (
+if(
   process.env.NODE_ENV === 'production' &&
   window.__REACT_DEVTOOLS_GLOBAL_HOOK__ &&
   Object.keys(window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers).length
